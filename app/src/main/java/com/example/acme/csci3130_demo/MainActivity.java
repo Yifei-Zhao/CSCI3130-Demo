21916aeadb0067413985d009e13801b2a9abeec0
yifeizhao <yf295016@dal.ca>
2018-05-16 22:49:48 -0300

    first commit

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,57 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText edit;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void buttonOnClick(View v) {
+        Button button = (Button) v;
+        edit = findViewById(R.id.editText);
+        text = findViewById(R.id.textView);
+        text.setText(edit.getText());
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
